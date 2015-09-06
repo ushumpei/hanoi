0d678966b444068ee002b22c12d91e24f8cb310a
ushumpei <shumpei.uzawa@gmail.com>
2015-09-07 02:18:27 +0900

    Change logic for functional
    関数型っぽく書いてみた

--- main.js
+++ main.js
@@ -10,6 +10,78 @@ var Hanoi = function(size) {
     this.size = size;
     this.discs = this.setDiscs();
 }
+
+/* Logic */
+// snapshotは[1's posision, 2's posision, .. , length's posision]のように,
+// 各大きさのディスクがどこにあるかを表現している.
+function click(size, count, direction) {
+    return getSnapshot(size, count + direction);
+}
+function getSnapshot(size, count) {
+    var snapshot = new Array(size);
+    for(var i = 0; i < size; i++) {
+        var discNumber = i + 1;
+        snapshot[i] = getDiscPosition(size, discNumber, count);
+    }
+    return snapshot;
+}
+function getDiscPosition(size, discNumber, count) {
+    var upperBound = Math.pow(2, discNumber - 1);
+    var range = Math.pow(2, discNumber);
+    var representative = count % (range * 3);
+    var position = 0;
+    var direction = getMoveDirection(size, discNumber);
+    if(representative < upperBound) {
+        position = 0;
+    } else if(representative < upperBound + range) {
+        position += direction * 1;
+    } else if(representative < upperBound + range * 2) {
+        position += direction * 2;
+    } else {
+        position = 0;
+    }
+    if(position < 0) {
+        position += 3;
+    }
+    return position;
+}
+// 1: 正の方向, -1: 負の方向
+function getMoveDirection(size, discNumber) {
+    return Math.pow(-1, size + discNumber + 1);
+}
+/* Projection */
+function projection(snapshot) {
+    var mainScreen = document.createElement("div");
+    for (var i = 0; i < snapshot.length; i++) {
+        var position = snapshot[i];
+        var disc = setupDisc(i + 1, position);
+        mainScreen.appendChild(disc);
+    }
+    return mainScreen;
+}
+
+function setupDisc(discNumber, position) {
+    var disc = document.createElement("div");
+    disc.innerText = discNumber;
+    disc.setAttribute("style", difineStyle(discNumber, position));
+    disc.setAttribute("class", "disc");
+    return disc;
+}
+function difineStyle(discNumber, position) {
+    var width = 3;
+    for (var i = 0; i < discNumber; i++) {
+        width += Math.pow(ratio, i);
+    }
+
+    var left = 50 * position;
+
+    var style = 
+          "width:" + width + "rem;"
+        + "left:" + left + "%;";
+    return style;
+}
+
+/* Legacy Codes */
 Hanoi.prototype.setDiscs = function() {
     clear();
     var discs = new Array();
@@ -61,10 +133,6 @@ function getDiscNumberToMove(count) {
         discNumber++;
     }
     return discNumber;
-}
-
-function getMoveDirection(size, discNumber) {
-    return Math.pow(-1,1+size+discNumber+1);
 }
 
 function rotateDisc(hanoi, direction, discNumber) {
@@ -128,3 +196,5 @@ function start() {
     stopButton = false;
     play();
 }
+
+
