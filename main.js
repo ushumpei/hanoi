ac87c91767eafc0f09f754c979745dad320d2648
ushumpei <shumpei.uzawa@gmail.com>
2015-09-08 00:52:16 +0900

    Delete unneccessary codes

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-var hanoi;
-var iterator;
-var count = 1;
-var positions = ["0", "1", "2"];
-var firstTerm = 3;
-var ratio = 4/5;
-var stopButton = false;
-
-var Hanoi = function(size) {
-    this.size = size;
-    this.discs = this.setDiscs();
-}
-
 /* Logic */
 // snapshotは[1's posision, 2's posision, .. , length's posision]のように,
 // 各大きさのディスクがどこにあるかを表現している.
@@ -87,14 +74,14 @@ function getDiscs(snapshot, towerNumber) {
             var discNumber = i;
             var disc = document.createElement("div");
             disc.innerText = discNumber + 1;
-            disc.setAttribute("style", difineStyle(discNumber, towerNumber));
+            disc.setAttribute("style", difineDiscStyle(discNumber, towerNumber));
             disc.setAttribute("class", "disc");
             discs.push(disc);
         }
     }
     return discs;
 }
-function difineStyle(discNumber, position) {
+function difineDiscStyle(discNumber, position) {
     var width = 3;
     for (var i = 0; i < discNumber; i++) {
         width += Math.pow(ratio, i);
@@ -106,120 +93,12 @@ function difineStyle(discNumber, position) {
     return style;
 }
 
-/* Legacy Codes */
-Hanoi.prototype.setDiscs = function() {
-    clear();
-    var discs = new Array();
-    for (var i = 0; i < this.size; i++) {
-        var disc = new Disc(i);
-        discs.push(disc);
-        disc.initializePosition();
-    }
-    return discs;
-}
-
-var Disc = function(number) {
-    this.number = number;
-    this.position = 0;
-}
-Disc.prototype.initializePosition = function() {
-    var position = this.position;
-    var label = this.number + 1;
-    var disc = document.createElement("div");
-
-    var width = firstTerm;
-    for (var i = 0; i < this.number; i++) {
-        width += Math.pow(ratio, i);
-    }
-
-    disc.innerText = label;
-    disc.setAttribute("class", "disc");
-    disc.setAttribute("id", "disc" + label);
-    disc.style.width = width + "rem";
-    var Tower = document.getElementById("tower" + positions[position]);
-    Tower.appendChild(disc);
-}
-Disc.prototype.changePosition = function(position) {
-    var from = this.position;
-    var to   = position;
-    this.position = to;
-    var label = this.number + 1;
-    var disc = document.getElementById("disc" + label);
-    var fromTower = document.getElementById("tower" + positions[from]);
-    fromTower.removeChild(disc);
-    var toTower = document.getElementById("tower" + positions[to]);
-    toTower.insertBefore(disc, toTower.firstChild);
-}
-
-function getDiscNumberToMove(count) {
-    var discNumber = 0;
-    while(count % 2 == 0) {
-        count = count / 2;
-        discNumber++;
-    }
-    return discNumber;
-}
-
-function rotateDisc(hanoi, direction, discNumber) {
-    var disc = hanoi.discs[discNumber];
-    var position = disc.position;
-    var newPosition = position + direction;
-    if (newPosition < 0) {
-        newPosition += 3;
-    }
-    if (newPosition > 2) {
-        newPosition -= 3;
-    }
-    disc.changePosition(newPosition);
-}
-
-function solve(hanoi) {
-        var discNumber = getDiscNumberToMove(count);
-        var direction = getMoveDirection(hanoi.size, discNumber);
-        rotateDisc(hanoi, direction, discNumber);
-        count++;
-}
-
-function initialize() {
-    build();
-}
-
-function clear() {
-    for (var i = 0; i < 3; i++) {
-        document.getElementById("tower" + positions[i]).innerHTML = "";
-    }
-}
-
+// Interface
 function build() {
-    var size = Number(document.getElementById("towerSize").value);
-    count = 1;
-    hanoi = new Hanoi(size);
-    stop();
-    play();
 }
-
 function reset() {
-    clear();
-    build();
-}
-
-function play() {
-    if(!stopButton && (count < Math.pow(2, hanoi.size))) {
-        solve(hanoi);
-        setTimeout(play, 500);
-    }
-}
-
-function stop() {
-    stopButton = true;
 }
 function next() {
-    solve(hanoi);
 }
-
-function start() {
-    stopButton = false;
-    play();
+function back() {
 }
-
-
